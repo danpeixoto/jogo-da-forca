6ed53fd5b06de9655c5a088e818880b6f831c36f
Daniel Peixoto Pinto da Silva <peixoto.daniel@hotmail.com>
2019-11-13 16:53:21 -0300

    Adicionado CORS a api

--- server/server.ts
+++ server/server.ts
@@ -1,53 +1,67 @@
-import * as restify from 'restify';
-import mongoose from 'mongoose';
-import {Router} from '../common/router';
-import {enviroment} from '../common/enviroment';
+import * as restify from "restify";
+import mongoose from "mongoose";
+import corsMiddleware from "restify-cors-middleware";
+import { Router } from "../common/router";
+import { enviroment } from "../common/enviroment";
 
 
 //Classe responsável por inicializar as rotas e o banco de dados da aplicação
-export class Server{
+export class Server {
     application !: restify.Server;
 
-    initializeDB(): Promise<any>{
-        (<any> mongoose).Promise = global.Promise;
-        return mongoose.connect(enviroment.db.url,{
-            useNewUrlParser:true,
+    initializeDB(): Promise<any> {
+        (<any>mongoose).Promise = global.Promise;
+        return mongoose.connect(enviroment.db.url, {
+            useNewUrlParser: true,
             useUnifiedTopology: true
         });
     }
 
 
-    initRoutes(routers: Router[]):Promise<any>{
-        return new Promise((resolve,reject)=>{
-            try{
+    initRoutes(routers: Router[]): Promise<any> {
+        return new Promise((resolve, reject) => {
+            try {
                 this.application = restify.createServer({
-                    name:'jogo-da-forca'
+                    name: "jogo-da-forca"
                 });
-            }catch(error){
+
+                const corsOptions: corsMiddleware.Options = {
+                    preflightMaxAge: 86400,//guarda por um dia 
+                    origins: ["*"],
+                    allowHeaders: ["authorization"],
+                    exposeHeaders: ["x-custom-header"]
+                }
+                const cors: corsMiddleware.CorsMiddleware = corsMiddleware(corsOptions);
+
+                this.application.pre(cors.preflight);
+                this.application.use(cors.actual);
+                //plugins
+                this.application.use(restify.plugins.bodyParser());
+                this.application.use(restify.plugins.queryParser());
+
+                //inicializa cada rota da aplicação.
+                for (let router of routers) {
+                    router.applyRoutes(this.application);
+                }
+
+                //tenta escutar na porta determinada, se conseguir ira executar a aplicação
+                this.application.listen(enviroment.server.port, () => {
+                    resolve(this.application);
+                });
+
+            } catch (error) {
                 reject(error);
             }
 
-            //plugins
-            this.application.use(restify.plugins.bodyParser());
-            this.application.use(restify.plugins.queryParser());
 
-            //inicializa cada rota da aplicação.
-            for(let router of routers){
-                router.applyRoutes(this.application);
-            }
-
-            //tenta escutar na porta determinada, se conseguir ira executar a aplicação
-            this.application.listen(enviroment.server.port,()=>{
-                resolve(this.application);
-            })
 
         });
     };
 
-    bootstrap(routers:Router[]=[]):Promise<Server>{
+    bootstrap(routers: Router[] = []): Promise<Server> {
         return this.initializeDB()
-        .then(()=>
-          this.initRoutes(routers).then(()=>this));
+            .then(() =>
+                this.initRoutes(routers).then(() => this));
     }
 
 };
